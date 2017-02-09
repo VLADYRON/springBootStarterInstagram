8f4fa39bf4929c4be0afff243e698bb71f4ccfba
woniper <leekw3747@gmail.com>
2017-02-10 08:53:20 +0900

    test code 변경

--- common/src/test/java/RestClientTest.java
+++ common/src/test/java/RestClientTest.java
@@ -1,12 +1,17 @@
-import info.thecodinglive.client.AuthInterCeptor;
+import info.thecodinglive.client.AuthInterceptor;
 import info.thecodinglive.client.RestClient;
+import info.thecodinglive.client.RestConfig;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by jins on 2017-02-06.
@@ -15,14 +20,16 @@ public class RestClientTest {
 
     @Test
     public void testAuth()throws IOException{
-        String targetUrl="https://api.instagram.com/oauth/access_token";
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(RestConfig.class);
+        AuthInterceptor authInterceptor = applicationContext.getBean(AuthInterceptor.class);
 
-        OkHttpClient client =  new RestClient.Builder().addNetworkInterceptor(new AuthInterCeptor()).build();
-
-        Request request = new Request.Builder()
-        .url(targetUrl).build();
-
+        String targetUrl="https://api.instagram.com/oauth/authorize";
+        OkHttpClient client =  new RestClient.Builder().addNetworkInterceptor(authInterceptor).build();
+        Request request = new Request.Builder().url(targetUrl).build();
         Response response = client.newCall(request).execute();
-        response.body().close();
+        ResponseBody body = response.body();
+
+        assertThat(response.code()).isEqualTo(200);
+        body.string();
     }
 }
